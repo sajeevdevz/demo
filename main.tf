c7152e51a003ba72f4f3b01b2ea206303a8555fa
sajeevdeva <sajeevdeva@gmail.com>
2023-11-26 09:16:21 +0000

    new terraform

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.81.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "myrg" {
+  name     = "var.resource_group_name"
+  location = "West Europe"
+}
+
+
+resource "azurerm_sql_server" "mysql" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.myrg.name
+  location                     = azurerm_resource_group.myrg.location
+  version                      = "12.0"
+  administrator_login          = var.admin_login
+  administrator_login_password = var.admin_password
+tags = {
+    "EA_Application_ID": "A2329",
+    "EA_Business_Platform": "",
+    "EA_Primary_Product": "events-platform",
+    "EA_Product_Portfolio": "",
+    "Environment": "Non Production",
+    "IOCode": "EC000442",
+    "Subscription_Type": "Cloud Native",  
+}
+}
+resource "azurerm_mssql_database" "test" {
+  name           = var.sql_db_name
+  server_id      = azurerm_sql_server.mysql.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+  tags = {
+    "EA_Application_ID": "A2329",
+    "EA_Business_Platform": "",
+    "EA_Primary_Product": "events-platform",
+    "EA_Product_Portfolio": "",
+    "Environment": "Non Production",
+    "IOCode": "EC000442",
+    "Subscription_Type": "Cloud Native",
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region in which to create resources"
+  type        = string
+}
+
+variable "admin_login" {
+  description = "The SQL Server administrator login"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The SQL Server administrator password"
+  type        = string
+}
+
+variable "sql_server_name" {
+  description = "The name of the SQL Server"
+  type        = string
+}
+
+
+variable "sql_db_name" {
+  description = "The name of the SQL Database"
+  type        = string
+}
