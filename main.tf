88575f0eea18a99761092f2afdd910622fac52fa
sajeevdeva <sajeevdeva@gmail.com>
2023-11-26 15:01:26 +0000

    test

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_mssql_database" "test" {
   server_id      = azurerm_sql_server.mysql.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
+  max_size_gb    = 1
   read_scale     = true
   sku_name       = "S0"
   zone_redundant = true
