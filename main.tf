bb96b1845a47b90ca14a7b57f43b1351442478f5
sajeevdeva <sajeevdeva@gmail.com>
2023-11-28 19:20:58 +0000

    new

--- main.tf
+++ main.tf
@@ -13,12 +13,16 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "new-RG"
+    resource_group_name  = "StorageAccount-ResourceGroup"
     storage_account_name = "abcd1234"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
+    use_oidc             = true
+    subscription_id      = "83d5cfeb-365c-424e-bff4-33ac28574f76"
+    tenant_id            = "c9e766e1-d0c5-4b4d-8dc9-d4e7c0615230"
   }
 }
+
 
 
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -8,4 +8,4 @@ Environment         =  "perf"
 # Environment-specific variables
 
 sql_server_name     = "mysqlsxss3sdsrvssedssss"
-sql_db_name         = "mysqldbfeccstssacscxxacsssfdsds"
+sql_db_name         = "mysqldbtssacscxxacsssfdsds"
